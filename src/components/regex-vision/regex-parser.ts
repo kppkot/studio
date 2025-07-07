755d3da36dc043a5b319cb98bcc15bb474cd8039
oleg ivanenko <arrowplume@gmail.com>
2025-07-07 21:15:24 +0000

    получил это SELECT\s+(?:(?!FROM).)*(?:FROM\s+([\w, ]+))(?:\s+WHERE\s+([

--- src/components/regex-vision/regex-parser.ts
+++ src/components/regex-vision/regex-parser.ts
@@ -287,7 +287,6 @@ function transformNodeToBlocks(node: any): Block[] {
 
         case 'WordBoundary':
           blockType = BlockType.ANCHOR;
-          // Make the check for 'negative' more robust against undefined values
           // The string MUST be '\\b' to represent the two characters \ and b, not the backspace control character.
           settings.type = node.negative === true ? '\\B' : '\\b';
           break;
